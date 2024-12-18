d8c2c1548dba120ad2125ac638c75818673ea210
kojimadev <kojimadev@gmail.com>
2024-12-18 11:56:26 +0900

    任意のWebAPIを実行して、取得した値を表形式で画面に表示する処理を追加

---
+++ src/BlazorWebAssemblySample/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorWebAssemblySample.Models
+{
+    /// <summary>
+    /// はてなブックマークとしてのエントリを表すクラスです。便宜的に記事という意味で使います。
+    /// </summary>
+    public class Article
+    {
+        public string Key { get; set; } = string.Empty;
+        public int Number { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public int BookmarkCount { get; set; }
+
+        /// <summary>
+        /// はてなブックマークのコメントが見れるページのURL
+        /// </summary>
+        public string EntryUrl { get; set; } = string.Empty;
+    }
+}
